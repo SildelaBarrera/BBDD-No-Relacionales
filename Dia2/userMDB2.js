63e90c8d0a2f2667b35fe1d060ec4906a71cc553
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-04-23 16:55:40 +0200

    Dia 2

---
+++ Dia2/userMDB2.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    marks: [],
+    subjects: [],
+    teachers: []
+})
+const MarkSchema = new mongoose.Schema({
+    date: Number,
+    mark: Number
+})
+const SubjectSchema = new mongoose.Schema({
+    title: String
+})
+const TeacherSchema = new mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    groups: String
+})
+
+const student = mongoose.model("Student", StudentSchema, "studentsReto2");
+const mark = mongoose.model("Mark", MarkSchema, "marksReto2");
+const subject = mongoose.model("Subject", SubjectSchema, "subjectsReto2");
+const teacher = mongoose.model("Teacher", TeacherSchema, "teachersReto2");
+
+module.exports = {student, mark, subject, teacher}
+ 
